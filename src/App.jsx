af5745f075e940e7d3ad472c3aefe1133438831e
Owenzo7 <owenlee545@gmail.com>
2023-05-28 17:44:02 +0300

    finished adding most of the components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import ImageLinkForm from "./Components/imageLinkForm";
 import Logo from "./Components/logo.component";
 import Navigation from "./Components/navigation.component";
+import Rank from "./Components/rank.component";
+import ParticlesBg from 'particles-bg'
 
 function App() {
   return (
     <div className="App">
+    <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <Facerecognition /> */}
     </div>
