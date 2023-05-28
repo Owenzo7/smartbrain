7e606b0aec472f3a35f480237d9ea28f54f2e590
Owenzo7 <owenlee545@gmail.com>
2023-05-28 16:31:49 +0300

    just added tachyons styling to project

---
+++ src/Components/faceRecognition.component.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Facerecognition() {
+  return (
+    <div>faceRecognition</div>
+  )
+}
+
+export default Facerecognition
---
+++ src/Components/navigation.component.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Navigation() {
+  return (
+    <nav style={{display: "flex", justifyContent: "flex-end"}}>
+
+    <p>Sign Out</p>
+    
+    </nav>
+  )
+}
+
+export default Navigation
